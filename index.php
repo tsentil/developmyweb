83b0d130d7a2766448fffec5989df9cece216779
Danny <t.senthilkanthan@gmail.com>
2013-01-16 22:38:48 +0000

    Moving original site to blueprint template

--- index.php
+++ index.php
@@ -12,56 +12,7 @@
 
 <body>
 	
-	<div id="siteWrapper">
-		
-		<div id="header">
-			<h1 title="Develop-my-web.co.uk"><span style="color:#fff;">&#123;</span> &nbsp;&nbsp;Develop-my-web<span class="header-logo">.co.uk</span> &nbsp;&nbsp;&#125;</span></h1>
-		</div>
-		
-		
-		
-		<div id="sidebar1">
-			<div id="menu1">
-				<ul>
-					<li>menu1</li>
-					<li>menu2</li>
-					<li>menu3</li>
-					<li>menu4</li>
-					<li>menu5</li>
-				</ul>
-			</div>
-		</div>
-		
-		<div id="main">
-		
-			<div class="ad-main">
-				<b>Place your adverts here!</b>
-			</div>
-
-			<div class="content">
-				<h1>Learn to develop your website</h1>
-				<p>The internet is a big place. It's used by people from all walks of life, looking for all sorts 
-				of things.</br>
-				</br>
-				If you have found this website, then you are looking for help on developing your website.</br>
-				</br>
-				Let's get started!
-				</p>
-			</div>
-			
-		</div>
-		
-		<div id="sidebar2">
-		<h2>Adverts</h2>
-		</div>
-		
-		<div class="clear"></div>
-		
-		<div id="footer">
-			Footer
-		</div>
-
-	</div>
+	
 </body>
 
 </html>
