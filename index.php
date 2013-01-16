6db95696f225a9c8f057f6362d44abf677c3ca58
Danny <t.senthilkanthan@gmail.com>
2013-01-16 22:35:26 +0000

    Blueprint template files

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <html>
 <head>
 	<title>Develop my web</title>
+	<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
+	<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print">
+	<!--[if lt IE 8]>
+		<link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
+	<![endif]-->
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<!--meta HTTP-EQUIV="REFRESH" CONTENT="3"-->
 </head>
